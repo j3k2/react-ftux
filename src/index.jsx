b7dcbcfd3d87c48aad545b9c9f5f14590ef8de84
John Kim <jksfo@pm.me>
2018-06-30 22:59:52 -0700

    setup package.json

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ReactFtux extends Component {
+  render() {
+    return (
+      <div>
+        react-ftux
+      </div>
+    );
+  }
+}
+
+export default ReactFtux;
