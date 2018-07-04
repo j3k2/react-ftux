c1e03c68aecd112bd30a94880d86434723c692f5
John Kim <jksfo@pm.me>
2018-07-04 14:53:50 -0700

    v0.1.1

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import ReactFtux from "../../lib";
+import { ReactFtux, ReactFtuxTooltip } from "../../lib";
 // import "./styles.css";
 
 function Demo() {
     return (
         <div>
             <h1>Demo with examples of the component</h1>
-            <ReactFtux></ReactFtux>
+            <ReactFtux total={4}></ReactFtux>
+            <ReactFtuxTooltip><button>asdf</button></ReactFtuxTooltip>
+
         </div>
     );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
+import EventEmitter from "wolfy87-eventemitter"
+
+const ee = new EventEmitter();
 
 class ReactFtux extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: this.props.total,
+      current: 0,
+      increaseStep: () => {
+        this.setState({ current: this.state.current + 1 })
+      }
+    };
+
+  }
+
+  componentDidMount() {
+    ee.on('increase', () => {
+      this.state.increaseStep();
+    });
+  }
+  
+  componentWillUnmount() {
+    ee.off('increase');
+  }
+
   render() {
     return (
       <div>
-        react-ftux
+        {this.state.current} / {this.state.total} ({JSON.stringify(this.state)})
       </div>
     );
   }
 }
 
-export default ReactFtux;
+class ReactFtuxTooltip extends Component {
+
+  increaseStep() {
+    ee.trigger('increase');
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        <button onClick={this.increaseStep}>+</button>
+      </div>
+    )
+  }
+}
+
+
+export { ReactFtux, ReactFtuxTooltip };
