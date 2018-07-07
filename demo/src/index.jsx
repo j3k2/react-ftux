9ca2d49b5596f03290953483f3e65ca06d0da0dc
John Kim <jksfo@pm.me>
2018-07-07 01:42:41 -0700

    state management with eventemitter

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -7,9 +7,17 @@ function Demo() {
     return (
         <div>
             <h1>Demo with examples of the component</h1>
-            <ReactFtux total={4}></ReactFtux>
-            <ReactFtuxTooltip><button>asdf</button></ReactFtuxTooltip>
-
+            <ReactFtux total={3}></ReactFtux>
+            <ReactFtuxTooltip step={0}><button>asdf</button></ReactFtuxTooltip>
+            <ReactFtuxTooltip step={1}><button style={{
+                background: "red",
+                // position: "fixed",
+                right: 0
+            }}>asdf</button></ReactFtuxTooltip>
+            <ReactFtuxTooltip step={2}><button style={{
+                background: "blue",
+                left: 20
+            }}>asdf</button></ReactFtuxTooltip>
         </div>
     );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,9 @@ class ReactFtux extends Component {
       total: this.props.total,
       current: 0,
       increaseStep: () => {
-        this.setState({ current: this.state.current + 1 })
+        const currentStep = this.state.current + 1;
+        this.setState({ current: currentStep });
+        ee.trigger('update', [currentStep, this.props.total]);
       }
     };
 
@@ -20,8 +22,13 @@ class ReactFtux extends Component {
     ee.on('increase', () => {
       this.state.increaseStep();
     });
+    ee.on('end', function(){
+      console.log('ftux end');
+    });
+    this.state.current = 0;
+    ee.trigger('update', [0, this.props.total]);
   }
-  
+
   componentWillUnmount() {
     ee.off('increase');
   }
@@ -36,16 +43,78 @@ class ReactFtux extends Component {
 }
 
 class ReactFtuxTooltip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false, 
+      last: false,
+      endFtux: () => {
+        ee.trigger('end');
+        this.setState({display: false});
+      },
+      increaseStep: () => {
+        ee.trigger('increase');
+      }
+    };
+  }
 
-  increaseStep() {
-    ee.trigger('increase');
+  checkCurrent(current, total) {
+    if (this.props.step === current) {
+      this.setState({
+        display: true
+      })
+    } else if (this.props.step === total - 1){
+      this.setState({
+        last: true
+      });
+      this.setState({
+        display: false
+      })
+    } else {
+      this.setState({
+        display: false
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.checkCurrent(0, 1);
+    ee.on('update', (current, total) => {
+      this.checkCurrent(current, total);
+    });
   }
 
   render() {
+    let buttons;
+
+    if(this.state.last) {
+      buttons = (<button onClick={this.state.endFtux}>
+        Finish
+      </button>)
+    } else {
+      buttons = (
+        <button 
+        onClick={this.state.increaseStep}>
+        +
+      </button>
+      )
+    }
+
+
     return (
       <div>
         {this.props.children}
-        <button onClick={this.increaseStep}>+</button>
+        {this.state.display ?  (
+          <div style={{
+            background: "black",
+            color: "white",
+            width: 80,
+            display: "inline",
+            position: "relative"
+          }}>[{this.props.step}]
+          {buttons}
+          </div>
+        ) : null}
       </div>
     )
   }
