3e0377661bf9a84c3d37f9a82f5f7ed7b06662f3
John Kim <jksfo@pm.me>
2019-11-20 15:16:31 -0800

    update docs/demo site

--- docs/src/index.jsx
+++ docs/src/index.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 import { ReactFtux, ReactFtuxTooltip } from "../../lib/bundle";
+import {
+    Container,
+    Dropdown,
+    Header,
+    Image,
+    Menu,
+    Icon
+} from 'semantic-ui-react'
+import '../assets/style.css';
+import paragraph from '../assets/paragraph.png';
 
 function Demo() {
     return (
@@ -11,203 +21,127 @@ function Demo() {
                 tooltipSettings={{
                     disableCloseButton: false,
                     disableKeydownListener: false,
-                    animationDuration: 0.4,
-                    tooltipWidth: 400
+                    animationDuration: 0.4
                 }}
                 ftuxEnd={() => {
                     console.log('ftux end');
                 }} />
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-                <span className="navbar-brand">
-                    Demo Application for <a href="https://github.com/j3k2/react-ftux">react-ftux</a>
-                </span>
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav navbar-sidenav" id="exampleAccordion">
-                        <li className="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-                            <a className="nav-link">
-                                <i className="fa fa-fw fa-dashboard"></i>
-                                <span className="nav-link-text">Link</span>
-                            </a>
-                        </li>
-                        <ReactFtuxTooltip
-                            step={3}
-                            pointerDirection="left">
-                            <h5>Step 3 tooltip</h5>
-                            <br />
-                            <span>This tooltip has the pointerDirection prop set to 'left'.</span>
-                        </ReactFtuxTooltip>
-                        <li className="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
-                            <a className="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
-                                <i className="fa fa-fw fa-file"></i>
-                                <span className="nav-link-text">Pages</span>
-                            </a>
-                            <ul className="sidenav-second-level collapse" id="collapseExamplePages">
-                                <li>
-                                    <a>Example Page</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav ml-auto">
-                        <ReactFtuxTooltip step={0}>
-                            <h5>Step 0 tooltip</h5>
-                            <br />
-                            <span>'ReactFtuxTooltip' component is included as a sibling to the menu element.</span>
-                        </ReactFtuxTooltip>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i className="fa fa-fw fa-envelope"></i>
-                                <span className="d-lg-none">Messages
-                        <span className="badge badge-pill badge-primary">12 New</span>
-                                </span>
-                                <span className="indicator text-primary d-none d-lg-block">
-                                    <i className="fa fa-fw fa-circle"></i>
-                                </span>
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="messagesDropdown">
-                                <h6 className="dropdown-header">New Messages:</h6>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item">
-                                    <strong>David Miller</strong>
-                                    <span className="small float-right text-muted">11:21 AM</span>
-                                    <div className="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end
-                            of the box so they don't overflow over to the sides!</div>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item">
-                                    <strong>Jane Smith</strong>
-                                    <span className="small float-right text-muted">11:21 AM</span>
-                                    <div className="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item">
-                                    <strong>John Doe</strong>
-                                    <span className="small float-right text-muted">11:21 AM</span>
-                                    <div className="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can
-                            discuss distribution.</div>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item small">View all messages</a>
-                            </div>
-                        </li>
-                        <ReactFtuxTooltip step={1}>
-                            <h5>Step 1 tooltip</h5>
-                            <br />
-                            <span>Clicking those menu elements still works as expected.</span>
-                        </ReactFtuxTooltip>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i className="fa fa-fw fa-bell"></i>
-                                <span className="d-lg-none">Alerts
-                            <span className="badge badge-pill badge-warning">6 New</span>
-                                </span>
-                                <span className="indicator text-warning d-none d-lg-block">
-                                    <i className="fa fa-fw fa-circle"></i>
-                                </span>
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="alertsDropdown">
-                                <h6 className="dropdown-header">New Alerts:</h6>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item">
-                                    <span className="text-success">
-                                        <strong>
-                                            <i className="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-                                    </span>
-                                    <span className="small float-right text-muted">11:21 AM</span>
-                                    <div className="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item">
-                                    <span className="text-danger">
-                                        <strong>
-                                            <i className="fa fa-long-arrow-down fa-fw"></i>Status Update</strong>
-                                    </span>
-                                    <span className="small float-right text-muted">11:21 AM</span>
-                                    <div className="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item">
-                                    <span className="text-success">
-                                        <strong>
-                                            <i className="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-                                    </span>
-                                    <span className="small float-right text-muted">11:21 AM</span>
-                                    <div className="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item small">View all alerts</a>
-                            </div>
-                        </li>
-                        <ReactFtuxTooltip
-                            step={2}
-                            pointerDirection="right">
-                            <h5>Step 2</h5>
-                            <br />
-                            <span>This tooltip has the pointerDirection prop set to 'right'.</span>
-                        </ReactFtuxTooltip>
-                        <li className="nav-item">
-                            <form className="form-inline my-2 my-lg-0 mr-lg-2">
-                                <div className="input-group">
-                                    <input className="form-control" type="text" placeholder="Search for..." />>
-                        <span className="input-group-append">
-                                        <button className="btn btn-primary" type="button">
-                                            <i className="fa fa-search"></i>
-                                        </button>
-                                    </span>
-                                </div>
-                            </form>
-                        </li>
-                        <ReactFtuxTooltip
-                            pointerDirection="right"
-                            scrollTo
-                            step={5}>
-                            <h5>Step 5 tooltip</h5>
-                            <br />
-                            <span>This tooltip has pointerDirection prop set to 'right' and the 'scrollTo' prop included.</span>
-                        </ReactFtuxTooltip>
-                        <li className="nav-item">
-                            <a className="nav-link">
-                                <i className="fa fa-fw fa-sign-out"></i>Logout</a>
-                        </li>
+            <Menu style={{ height: 61}} fixed='top' inverted>
+                <Menu.Item header>
+                    <a href="https://github.com/j3k2/react-ftux">react-ftux demo</a>
+                </Menu.Item>
+                <Menu.Item as='a'>Home</Menu.Item>
+                <Menu.Item as='a'>Users</Menu.Item>
 
-                    </ul>
-                </div>
-            </nav>
-            <div className="content-wrapper" style={{ padding: 40 }}>
-                <p style={{ height: 2000 }}>
-                    <h1>Header</h1>
-                </p>
+                
+                  <ReactFtuxTooltip step={0}>
+                    <h5>Step 0 tooltip</h5>
+                    <br />
+                    <p>The 'ReactFtuxTooltip' component is included as a sibling to the menu element. Click buttons, press arrow keys, or press Enter/Backspace keys to navigate through the tour. Esc key ends the tour.</p>
+                </ReactFtuxTooltip>
+                <Menu.Item as='a'>Pages</Menu.Item>
+
+                <ReactFtuxTooltip step={1}>
+                    <h5>Step 1 tooltip</h5>
+                    <br />
+                    <p className='tooltip-text'>
+                        Font styles for the buttons and tooltip content are not 
+                        inherited from their parent elements, and can be controlled 
+                        by the 'fontStyle' prop on the 'ReactFtux' component. 
+
+                        App style rules can still override these defaults for any
+                        child elements of 'ReactFtuxTooltip'.
+                        
+                        Note class 'tooltip-text' applied to this {'<p>'} element.</p>
+                </ReactFtuxTooltip>
+                <Menu.Item as='a'>Settings</Menu.Item>
+
+                <ReactFtuxTooltip zIndex={1} step={2} >
+                    <h5>Step 2 tooltip</h5>
+                    <br />
+                    <p>The 'zIndex' prop is set to {1} for this tooltip to preserve dropdown interaction.</p>
+                </ReactFtuxTooltip>
+                <Dropdown item simple text='Dropdown'>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Header>Header Item</Dropdown.Header>
+                        <Dropdown.Item>
+                            <i className='dropdown icon' />
+                            <span className='text'>Submenu</span>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>List Item</Dropdown.Item>
+                                <Dropdown.Item>List Item</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown.Item>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu>
+
+            <Menu style={{ top: 61 }} fixed="left" vertical inverted>
+                    <ReactFtuxTooltip
+                        pointerDirection='left'
+                        offsetLeft={-160}
+                        offsetTop={-10}
+                        step={3}>
+                            <h5>Step 3 tooltip</h5>
+                            <br/>
+                            <p>
+                            This tooltip has the 'pointerDirection' prop set to 'left', 'offsetLeft' prop set to '-160', and 'offsetTop' prop set to '-10' to adjust position of tooltip/pointer.
+                            </p>
+                        </ReactFtuxTooltip>
+                    <Menu.Item
+                        name='Link'
+                    />
+                    <Menu.Item
+                        name='More'
+                    />
+                    <Menu.Item
+                        name='Other '
+                    />
+                </Menu>
+            <Container text style={{ marginTop: '7em', paddingLeft: '15rem' }}>
+
+            <ReactFtuxTooltip scrollToTop step={5}>
+                    <h5>Step 5 tooltip</h5>
+                    <br />
+                    <p>This tooltip has the 'scrollToTop' prop included, which is preferred over the 'scrollTo' prop for elements at the top of scrollable content.</p>
+                </ReactFtuxTooltip>
+                <Header as='h1'>react-ftux demo</Header>
+                <Image src={paragraph} style={{ marginTop: '2em' }} />
+                <Image src={paragraph} style={{ marginTop: '2em' }} />
+                <Image src={paragraph} style={{ marginTop: '2em' }} />
+                <Image src={paragraph} style={{ marginTop: '2em' }} />
+                <Image src={paragraph} style={{ marginTop: '2em' }} />
+                <Image src={paragraph} style={{ marginTop: '2em' }} />
+                <br/>
+                <br/>
                 <ReactFtuxTooltip
                     scrollTo
-                    step={4}
-                    pointerDirection="below">
-                    <h5>Step 4 tooltip</h5>
-                    <br />
-                    <span>This tooltip has pointerDirection set to 'below'. Also has the 'scrollTo' prop, which makes the window scroll to the element's position.</span>
-                </ReactFtuxTooltip>
-                <div style={{
-                    border: 'solid 1px black',
-                    width: 180,
-                    padding: 8,
-                    borderRadius: 3
-                }}>
-                    <i style={{ fontSize: 28 }} className="fa fa-fw fa-share-square-o"></i>
-                    <a>
-                        <i style={{ fontSize: 32 }} className="fa fa-fw fa-envelope"></i>
-                    </a>
-                    <a>
-                        <i style={{ fontSize: 32 }} className="fa fa-fw fa-facebook-official"></i>
-                    </a>
-                    <a>
-                        <i style={{ fontSize: 32 }} className="fa fa-fw fa-twitter-square"></i>
-                    </a>
-                </div>
-            </div>
-        </div >
+                    pointerDirection='below'
+                    step={4}>
+                        <h5>Step 4 tooltip</h5>
+                        <br/>
+                        <p>
+                        This tooltip has the 'scrollTo' prop included and the 'pointerDirection' prop set to 'below'.
+                        </p>
+                        </ReactFtuxTooltip>
+                <Menu compact>
+                    <Menu.Item>
+                        <Icon name='twitter' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Icon name='facebook' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Icon name='mail' />
+                    </Menu.Item>
+                </Menu>
+            </Container>
+        </div>
+
     );
 }
 
